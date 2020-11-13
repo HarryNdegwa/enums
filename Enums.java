74f6b467f9365f61bb6d4470329871937f6d69a4
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-13 16:40:05 +0300

    Enums get a little bit interesting

--- Enums.java
+++ Enums.java
@@ -3,16 +3,47 @@
 public class Enums {
 
     public enum Direction {
-        NORTH, SOUTH, EAST, WEST;
+        NORTH("N"), SOUTH("S"), EAST("E"), WEST("W");
+
+        private final String shortCode;
+
+        Direction(String code) {
+            this.shortCode = code;
+        }
+
+        public String getShortCode() {
+            return this.shortCode;
+        }
 
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to enums");
 
-        Direction north = Direction.NORTH;
+        Direction dir = Direction.NORTH;
 
-        System.out.println("The direction is " + north);
+        System.out.println("The direction is " + dir);
+
+        System.out.println();
+
+        switch (dir) {
+            case NORTH:
+                System.out.println("Direction is north");
+                break;
+            case EAST:
+                System.out.println("Direction is east");
+                break;
+            case WEST:
+                System.out.println("Direction is west");
+                break;
+            case SOUTH:
+                System.out.println("Direction is south");
+                break;
+            default:
+                System.out.println("Helloo");
+                break;
+
+        }
     }
 
 }
